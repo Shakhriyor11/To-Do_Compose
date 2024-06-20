2749627652ae6a9b3ebe8e45bb75f3c6bff77934
Shakhriyor11 <shaxrik113099@gmail.com>
2024-06-21 00:15:59 +0500

    - created searchAppBar

--- app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/list/ListAppBar.kt
@@ -1,8 +1,13 @@
 package com.example.to_docompose.ui.screens.list
 
 //import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -11,14 +16,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.ContentAlpha
 import com.example.to_docompose.R
 import com.example.to_docompose.components.PriorityItem
 import com.example.to_docompose.data.models.Priority
 import com.example.to_docompose.ui.theme.LARGE_PADDING
+import com.example.to_docompose.ui.theme.TOP_APP_BAR_HEIGHT
 import com.example.to_docompose.ui.theme.ToDoComposeTheme
 import com.example.to_docompose.ui.theme.Typography
 
@@ -143,6 +154,81 @@ fun DeleteAllAction(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchAppBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT),
+        shadowElevation = 4.dp,
+        color = MaterialTheme.colorScheme.surface
+
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = TextFieldDefaults.textFieldColors(
+                cursorColor = MaterialTheme.colorScheme.onSurface,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                containerColor = MaterialTheme.colorScheme.surface
+            ),
+            value = text,
+            onValueChange = onTextChange,
+            placeholder = {
+                Text(
+                    modifier = Modifier
+                        .alpha(ContentAlpha.medium),
+                    text = "Search",
+                    color = Color.White
+                )
+            },
+            textStyle = TextStyle(
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = MaterialTheme.typography.titleMedium.fontSize
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(onClick = {  }) {
+                    Icon(
+                        modifier = Modifier
+                            .alpha(ContentAlpha.disabled),
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search Icon",
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { onCloseClicked() }) {
+                    Icon(
+                        modifier = Modifier
+                            .alpha(ContentAlpha.disabled),
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = "Close Icon",
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchClicked(text)
+                }
+            )
+        )
+    }
+}
+
 
 @Composable
 @Preview(showBackground = true)
@@ -167,4 +253,30 @@ fun DefaultListAppBarDarkPreview() {
         )
     }
 
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SearchAppBarPreview() {
+    ToDoComposeTheme(darkTheme = false) {
+        SearchAppBar(
+            text = "search",
+            onTextChange = {},
+            onCloseClicked = {},
+            onSearchClicked = {}
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SearchAppBarPreviewDark() {
+    ToDoComposeTheme(darkTheme = true) {
+        SearchAppBar(
+            text = "search",
+            onTextChange = {},
+            onCloseClicked = {},
+            onSearchClicked = {}
+        )
+    }
 }
