0a188ba94ceb28faf6b9d41f6f5e186977b91d21
Shakhriyor11 <shaxrik113099@gmail.com>
2024-07-04 17:26:15 +0500

    - added animation to splash screen

--- app/src/main/java/com/example/to_docompose/ui/screens/splash_screen/SplashScreen.kt
+++ app/src/main/java/com/example/to_docompose/ui/screens/splash_screen/SplashScreen.kt
@@ -1,16 +1,25 @@
 package com.example.to_docompose.ui.screens.splash_screen
 
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,8 +33,25 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     navigateToListScreen: () -> Unit
 ) {
-    
+
+    var startAnimation by remember {
+        mutableStateOf(false)
+    }
+    val offsetState by animateDpAsState(
+        targetValue = if (startAnimation) 0.dp else 100.dp,
+        animationSpec = tween(
+            durationMillis = 1000
+        ), label = ""
+    )
+    val alphaState by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 1000
+        ), label = ""
+    )
+
     LaunchedEffect(key1 = true) {
+        startAnimation = true
         delay(SPLASH_SCREEN_DELAY)
         navigateToListScreen()
     }
@@ -37,7 +63,10 @@ fun SplashScreen(
         contentAlignment = Alignment.Center
     ) {
         Image(
-            modifier = Modifier.size(100.dp),
+            modifier = Modifier
+                .size(100.dp)
+                .offset(y = offsetState)
+                .alpha(alpha = alphaState),
             painter = painterResource(id = getLogo()),
             contentDescription = ""
         )
